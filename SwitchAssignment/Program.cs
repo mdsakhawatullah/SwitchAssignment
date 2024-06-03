82371d2b9cebc7fd272aac546ca9c759baa88d3b
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-06-03 21:54:35 +0600

    added

---
+++ SwitchAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SwitchAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            switch(name)
+            {
+                case "nine":
+                    Console.WriteLine("This is class nine");
+                    break;
+
+                case "ten":
+                    Console.WriteLine("This is class ten");
+                    break;
+
+                case "six":
+                    Console.WriteLine("This is class six");
+                    break;
+                default: 
+                    Console.WriteLine("This is not in our range");
+                    break;
+            }
+        }
+    }
+}
